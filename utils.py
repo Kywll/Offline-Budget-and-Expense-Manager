df53d00c63670261adaab34155fb48ebdf827b4d
Kywll <mkywll74@gmail.com>
2025-11-26 23:12:24 +0800

    added search function on utils

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 class Util():
 
 
-    def search(self):
-        pass
+    def search(self, object, data, target):
+        for i in range(len(object[data])):
+            if object[data][i] == target:
+                return object[data][i]
+        return None
+        
     def insert(self):
         pass
     def delete(self):
