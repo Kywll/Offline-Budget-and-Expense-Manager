7f06895c4b05e33dbc64022260612818222b9242
Kywll <mkywll74@gmail.com>
2025-11-29 20:32:45 +0800

    fixed search and update function. Implemented delete function

--- utils.py
+++ utils.py
@@ -4,15 +4,15 @@ class Util():
     def search(self, object, data, target):
         for i in range(len(object)):
             if object[i][data] == target:
-                return object[i][data]
+                return object[i]
         return None
         
     def insert(self, object, data):
         object.append(data)
-    def delete(self):
-        pass
+    def delete(self, object, data, target):
+        object.remove(self.search(object, data, target))
     def update(self, object, data, new_data, target):
-        self.search(object, data, target) = new_data
+        self.search(object, data, target)[data] = new_data
     def sort(self):
         pass
 
